fb7bcc5f0be793149206aa60746e8f01873325af
hargoniX <hargonix@gmail.com>
2018-03-27 17:31:22 +0200

    removed debugging ouptut

--- cloudlink/configure.py
+++ cloudlink/configure.py
@@ -2,7 +2,6 @@ with open("cloudlink.ino") as main:
     main = main.readlines()
 
 debug = input("Enable debuggin?(y/n): ").lower()
-print(main)
 if debug == "n":
     main[main.index("#define DEBUG\n")] = "//#define DEBUG"
 
