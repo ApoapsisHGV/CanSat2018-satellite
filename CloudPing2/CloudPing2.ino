99d33f14972b24ec7492cdf91046b5d68a7431c8
hargoniX <hargonix@gmail.com>
2018-03-30 22:34:02 +0200

    rename to cloudping

--- CloudPing2/CloudPing2.ino
+++ CloudPing2/CloudPing2.ino
@@ -2,11 +2,12 @@
 #include <SPI.h>
 
 
-const int sdPin = 4;
+const int sdPin = 10;
 
 void setup(){
   Serial.begin(9600);
   if(!SD.begin(sdPin)){
+    Serial.println("init failed");
     while(1);  
   }
 }
@@ -14,8 +15,11 @@ void setup(){
 void loop(){
   File logFile;
   if(Serial.available()){
+    String data;
     logFile = SD.open("log.txt", FILE_WRITE);
-    logFile.println(Serial.readString());
+    data = Serial.readString();
+    Serial.println(data);
+    logFile.println(data);
     logFile.close();
   }
   delay(500);
