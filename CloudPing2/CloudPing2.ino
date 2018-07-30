c7709838c1efabd454b4aaf3cee829ca9e0ec02d
Niklas Cölle <35871438+NiklasC@users.noreply.github.com>
2018-07-30 23:12:26 +0200

    Checks für log.txt und Debug Funktion

--- CloudPing2/CloudPing2.ino
+++ CloudPing2/CloudPing2.ino
@@ -1,16 +1,21 @@
 #include <SD.h>
 #include <SPI.h>
 
-
+const boolean debug = false;
 const int sdPin = 10;
 
 void setup(){
   Serial.begin(115200);
+  delay(1000);
   if(!SD.begin(sdPin)){
-    Serial.println("init failed");
+    Serial.println("Initialisierung fehlgeschlagen - SD Karte vorhanden?");
     while(1);  
   }
-  Serial.println("init succesfull");
+  if(SD.exists("log.txt")){
+    Serial.println("log.txt wird gelöscht");
+    SD.remove("log.txt");
+  }
+  Serial.println("Initialisierung erfolgreich");
 }
 
 void loop(){
@@ -19,7 +24,9 @@ void loop(){
     String data;
     logFile = SD.open("log.txt", FILE_WRITE);
     data = Serial.readString();
-    Serial.println(data);
+    if(debug){
+      Serial.println(data);
+    }
     logFile.println(data);
     logFile.close();
   }
